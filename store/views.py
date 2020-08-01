63d1f46d8f1a01f572e1a8ae1a43e30d29dca92d
sahil <sahil13399@gmail.com>
2020-08-01 11:33:29 +0530

    Models added , Cart and checkout rendered

--- store/views.py
+++ store/views.py
@@ -1,14 +1,39 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def main(request):
     return render(request,'store/main.html')
 
 def cart(request):
-    return render(request,'store/cart.html')
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		#Create empty cart for now for non-logged in user
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0}
+
+	context = {'items':items, 'order':order}
+	return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    return render(request,'store/checkout.html')
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		#Create empty cart for now for non-logged in user
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0}
 
+	context = {'items':items, 'order':order}
+	return render(request, 'store/checkout.html', context)
+
+    
 def store(request):
-    return render(request,'store/store.html')
+    products=Product.objects.all()
+    context={'products':products}
+    return render(request,'store/store.html',context)
